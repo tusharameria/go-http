f04cd56a13eb75b6f9de13bdddcb9e2f0ec568b5
Tushar Ameria <tusharameria@gmail.com>
2025-09-22 18:25:15 +0530

    multiple connection open and close handling

--- cmd/main.go
+++ cmd/main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"net"
 	"os"
+	"sync/atomic"
 )
 
 func main() {
@@ -16,17 +17,44 @@ func main() {
 	fmt.Printf("Listener Speifications : %+v\n", listener)
 	fmt.Printf("Listener Address : %+v\n", listener.Addr())
 
+	var connCount int64 = 0
+
 	for {
-		connection, err := listener.Accept()
+		conn, err := listener.Accept()
 		if err != nil {
 			fmt.Printf("%s\n", err)
+			continue
 		}
+		atomic.AddInt64(&connCount, 1)
+		fmt.Println("New Client Connected...")
+		fmt.Printf("Local address : %+v\n", conn.LocalAddr())
+		fmt.Printf("Active Connections : %d\n", connCount)
 
-		fmt.Printf("New client connected, local address : %+v\n", connection.LocalAddr())
-		fmt.Printf("New client connected, remote address : %+v\n", connection.RemoteAddr())
-
-		connection.Write([]byte("Hello from minimal TCP server\n"))
-
-		connection.Close()
+		go handleConnection(conn, &connCount)
+		conn.Write([]byte("Hello from minimal TCP server...\n"))
 	}
+}
+
+func handleConnection(conn net.Conn, connCount *int64) {
+	defer closeConnection(conn, connCount)
+
+	buff := make([]byte, 1024)
+	num := *connCount
+	for {
+		n, err := conn.Read(buff)
+		if err != nil {
+			fmt.Printf("%s\n", err)
+			return
+		}
+		fmt.Printf("Received from Client %d\n", num)
+		fmt.Printf("Message : %s", buff[:n])
+
+		conn.Write([]byte(fmt.Sprintf("You sent : %s", buff[:n])))
+	}
+}
+
+func closeConnection(conn net.Conn, connCount *int64) {
+	newVal := atomic.AddInt64(connCount, -1)
+	fmt.Printf("Active Connections after closing connection : %d\n", newVal)
+	conn.Close()
 }
