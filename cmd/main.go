b471945b35c795634f6ed7ce2176e60c27e4a8d3
Tushar Ameria <tusharameria@gmail.com>
2026-05-22 19:57:32 +0530

    refactor logic with a function which returns a channel

--- cmd/main.go
+++ cmd/main.go
@@ -8,38 +8,51 @@ import (
 	"os"
 )
 
+func getLinesChannel(f io.ReadCloser) <-chan string {
+	out := make(chan string, 1)
+
+	go func() {
+		defer f.Close()
+		defer close(out)
+		// Read the file till it reaches the end
+		s := ""
+		for {
+			data := make([]byte, 8)
+			n, err := f.Read(data)
+			if err != nil {
+				// Always check for End of File while reading a file to properly close it
+				if err == io.EOF {
+					break
+				}
+				log.Fatal(err)
+			}
+			data = data[:n]
+			// Find index of a byte in a byte array
+			if i := bytes.IndexByte(data, '\n'); i != -1 {
+				s += string(data[:i])
+				out <- s
+				data = data[i+1:]
+				s = ""
+			}
+			s += string(data)
+		}
+		if len(s) != 0 {
+			out <- s
+		}
+	}()
+
+	return out
+}
+
 func main() {
 	// Open the file for reading
 	file, err := os.Open("messages.txt")
 	if err != nil {
 		log.Fatal(err)
 	}
-	// Always close the file when we're done using defer
-	defer file.Close()
 
-	// Read the file till it reaches the end
-	s := ""
-	for {
-		data := make([]byte, 8)
-		n, err := file.Read(data)
-		if err != nil {
-			// Always check for End of File while reading a file to properly close it
-			if err == io.EOF {
-				break
-			}
-			log.Fatal(err)
-		}
-		data = data[:n]
-		// Find index of a byte in a byte array
-		if i := bytes.IndexByte(data, '\n'); i != -1 {
-			s += string(data[:i])
-			fmt.Printf("read in : %s\n", s)
-			data = data[i+1:]
-			s = ""
-		}
-		s += string(data)
+	for str := range getLinesChannel(file) {
+		fmt.Printf("read : %s\n", str)
 	}
-	if len(s) != 0 {
-		fmt.Printf("read : %s\n", s)
-	}
+
 }
