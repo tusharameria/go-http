ac4acae055269bdf1799c1216b1dafb07fb2b4c4
Tushar Ameria <tusharameria@gmail.com>
2026-07-03 22:26:21 +0530

    Request Lines - Finished

--- cmd/tcplistener/main.go
+++ cmd/tcplistener/main.go
@@ -1,48 +1,12 @@
 package main
 
 import (
-	"bytes"
 	"fmt"
-	"io"
 	"log"
 	"net"
+
+	"github.com/tusharameria/go-http/cmd/request"
 )
-
-func getLinesChannel(f io.ReadWriteCloser) <-chan string {
-	out := make(chan string, 1)
-
-	go func() {
-		defer f.Close()
-		defer close(out)
-		// Read the file till it reaches the end
-		s := ""
-		for {
-			data := make([]byte, 8)
-			n, err := f.Read(data)
-			if err != nil {
-				// Always check for End of File while reading a file to properly close it
-				if err == io.EOF {
-					break
-				}
-				log.Fatal(err)
-			}
-			data = data[:n]
-			// Find index of a byte in a byte array
-			if i := bytes.IndexByte(data, '\n'); i != -1 {
-				s += string(data[:i])
-				out <- s
-				data = data[i+1:]
-				s = ""
-			}
-			s += string(data)
-		}
-		if len(s) != 0 {
-			out <- s
-		}
-	}()
-
-	return out
-}
 
 func main() {
 	listener, err := net.Listen("tcp", ":42069")
@@ -55,8 +19,13 @@ func main() {
 		if err != nil {
 			log.Println("error in Accept", "err", err)
 		}
-		for line := range getLinesChannel(conn) {
-			fmt.Printf("read : %s\n", line)
+		r, err := request.RequestFromReader(conn)
+		if err != nil {
+			log.Println("error in RequestFromReader", "err", err)
 		}
+		fmt.Println("Request Line :")
+		fmt.Printf("- Method: %v\n", r.RequestLine.Method)
+		fmt.Printf("- Target: %v\n", r.RequestLine.RequestTarget)
+		fmt.Printf("- Version: %v\n", r.RequestLine.HttpVersion)
 	}
 }
