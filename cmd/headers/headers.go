9be647327356941ef2261d7a142ebfcac815bf9c
Tushar Ameria <tusharameria@gmail.com>
2026-07-04 18:31:59 +0530

    Chapter 5: HTTP Headers, L2: Constraints

--- cmd/headers/headers.go
+++ cmd/headers/headers.go
@@ -3,19 +3,46 @@ package headers
 import (
 	"bytes"
 	"fmt"
+	"strings"
 )
 
-type Headers map[string]string
+func isToken(str []byte) bool {
+	for _, ch := range str {
+		if ch >= 'A' && ch <= 'Z' ||
+			ch >= 'a' && ch <= 'z' ||
+			ch >= '0' && ch <= '9' {
+			continue
+		}
+		switch ch {
+		case '!', '#', '$', '%', '&', '*', '+', '-', '.', '^', '_', '`', '|', '~':
+			continue
+		}
+		return false
+	}
+	return true
+}
+
+type Headers struct {
+	header map[string]string
+}
 
 var rn = []byte("\r\n")
 
-func NewHeaders() Headers {
-	return make(Headers)
+func NewHeaders() *Headers {
+	return &Headers{
+		header: make(map[string]string),
+	}
+}
+
+func (h *Headers) Get(name string) string {
+	return h.header[strings.ToLower(name)]
+}
+
+func (h *Headers) Set(name, value string) {
+	h.header[strings.ToLower(name)] = value
 }
 
 func parseHeader(fieldLine []byte) (string, string, error) {
-	fmt.Println("==== parseHeader ====")
-	fmt.Println(string(fieldLine))
 	parts := bytes.SplitN(fieldLine, []byte(":"), 2)
 	if len(parts) != 2 {
 		return "", "", fmt.Errorf("malformed field-line")
@@ -25,11 +52,12 @@ func parseHeader(fieldLine []byte) (string, string, error) {
 	if fieldName[0] == ' ' || fieldName[len(fieldName)-1] == ' ' {
 		return "", "", fmt.Errorf("malformed field-name")
 	}
+
 	fieldValue = bytes.TrimSpace(fieldValue)
 	return string(fieldName), string(fieldValue), nil
 }
 
-func (h Headers) Parse(data []byte) (int, bool, error) {
+func (h *Headers) Parse(data []byte) (int, bool, error) {
 	read := 0
 	done := false
 
@@ -48,9 +76,14 @@ func (h Headers) Parse(data []byte) (int, bool, error) {
 
 		fieldName, fieldValue, err := parseHeader(data[read : read+idx])
 		if err != nil {
-			return read, done, err
+			return 0, false, err
 		}
-		h[fieldName] = fieldValue
+
+		if !isToken([]byte(fieldName)) {
+			return 0, false, fmt.Errorf("malformed header name")
+		}
+
+		h.Set(fieldName, fieldValue)
 		read += idx + len(rn)
 	}
 	return read, done, nil
--- cmd/headers/headers_test.go
+++ cmd/headers/headers_test.go
@@ -16,14 +16,21 @@ func TestHeaderParse(t *testing.T) {
 	fmt.Println(n)
 	require.NoError(t, err)
 	require.NotNil(t, headers)
-	assert.Equal(t, "localhost:42069", headers["Host"])
-	assert.Equal(t, "barbar", headers["FooFoo"])
+	assert.Equal(t, "localhost:42069", headers.Get("Host"))
+	assert.Equal(t, "barbar", headers.Get("FooFoo"))
 	assert.Equal(t, 41, n)
 	assert.False(t, done)
 
 	// Test: Invalid spacing header
 	headers = NewHeaders()
 	data = []byte("       Host: localhost:42069\r\n\r\n")
+	n, done, err = headers.Parse(data)
+	require.Error(t, err)
+	assert.Equal(t, 0, n)
+	assert.False(t, done)
+
+	headers = NewHeaders()
+	data = []byte("H©st: localhost:42069\r\n\r\n")
 	n, done, err = headers.Parse(data)
 	require.Error(t, err)
 	assert.Equal(t, 0, n)
