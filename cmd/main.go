74924d76ce5eca87d9133dedc5ba85038d58baa3
Tushar Ameria <tusharameria@gmail.com>
2026-07-30 22:31:38 +0530

    Emulate short byte chunks using bufferLen == 8 bytes

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"bytes"
 	"fmt"
 	"log"
 	"net"
@@ -22,17 +21,15 @@ func main() {
 	}
 	defer conn.Close()
 
-	buffer := make([]byte, 1024)
+	bufferLen := 8
+	buffer := make([]byte, bufferLen)
+	msg := ""
 	for {
-		_, err := conn.Read(buffer)
+		n, err := conn.Read(buffer)
 		if err != nil {
 			break
 		}
-		newLineIdx := bytes.LastIndex(buffer, []byte("\n"))
-		if newLineIdx == -1 {
-			continue
-		}
-		msg := string(buffer[:newLineIdx])
+		msg = string(buffer[:n])
 		fmt.Printf("Msg Received : %s\n", msg)
 
 		conn.Write([]byte(fmt.Sprintf("Response from server : Hey you sent us \"%s\"\n", msg)))
