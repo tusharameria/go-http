d2eb42f6c9f2e45218e40099bd6646cb118e2c99
Tushar Ameria <tusharameria@gmail.com>
2026-07-31 18:06:35 +0530

    Parser separation

---
+++ internal/parser/parser.go
@@ -0,0 +1,24 @@
+package parser
+
+import (
+	"bytes"
+)
+
+type Parser struct {
+	aggregator []byte
+}
+
+func (p *Parser) Feed(data []byte) [][]byte {
+	res := [][]byte{}
+	p.aggregator = append(p.aggregator, data...)
+	for {
+		firstIdx := bytes.IndexByte(p.aggregator, '\n')
+		if firstIdx >= 0 {
+			res = append(res, p.aggregator[:firstIdx])
+		} else {
+			break
+		}
+		p.aggregator = p.aggregator[firstIdx+1:]
+	}
+	return res
+}
--- internal/server/handler.go
+++ internal/server/handler.go
@@ -1,26 +1,25 @@
 package server
 
 import (
-	"bytes"
 	"fmt"
 	"net"
+
+	"github.com/tusharameria/go-http/internal/parser"
 )
 
 func (s *Server) handleConnection(conn net.Conn) error {
 	bufferLen := 8
 	buffer := make([]byte, bufferLen)
-	var aggregator []byte
+	p := parser.Parser{}
 	for {
 		n, err := conn.Read(buffer)
 		if err != nil {
 			return err
 		}
-		aggregator = append(aggregator, buffer[:n]...)
-		lastIdx := bytes.Index(aggregator, []byte("\n"))
-		if lastIdx >= 0 {
-			fmt.Printf("Full Text : %s\n", aggregator[:lastIdx])
-			conn.Write([]byte(fmt.Sprintf("Response from server : Hey you sent us \"%s\"\n", aggregator[:lastIdx])))
-			aggregator = aggregator[lastIdx+1:]
+		res := p.Feed(buffer[:n])
+		for _, line := range res {
+			fmt.Printf("Received : %s\n", line)
+			conn.Write([]byte(fmt.Sprintf("You sent : %s\n", line)))
 		}
 	}
 }
